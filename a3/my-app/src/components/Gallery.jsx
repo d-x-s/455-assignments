ce50de98376145063c1385701ed3ef46ca24be42
Davis Song <davissong01@gmail.com>
2023-06-15 14:37:00 -0700

    A3(chore): copy over A2 code

---
+++ a3/my-app/src/components/Gallery.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import Card from './Card';
+
+const galleryStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    marginLeft: '-10px', // Negative margin on the left
+    marginRight: '-10px', // Negative margin on the right
+};
+
+function Gallery({ cards }) {
+    return (
+        <div style={galleryStyle}>
+        {/* Render cards based on the items array */}
+        {cards.map(card => (
+          <Card
+            key={card.key}
+            price={card.price} // Make sure to provide a unique key prop for each item
+            description={card.description}
+            itemName={card.name}
+            imageUrl={card.imageUrl}
+          />
+        ))}
+      </div>
+    )
+}
+
+// Map the state from Redux store to component props
+const mapStateToProps = state => ({
+    cards: state.form.items // Assuming `items` is the property name in your Redux store
+  });
+  
+// Connect the Gallery component to the Redux store
+export default connect(mapStateToProps)(Gallery);
---
+++ a3/my-app/src/components/Navbar.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <div className="Navbar">
+        <nav className="navbar">
+            <div className="logo">
+                <img src={logo} className="pepe-logo" alt="The logo!!!"/>
+            </div>
+            <div className="header-links">
+                <Link to="/home">counter</Link>
+                <Link to="/about">form</Link>
+                <Link to="/contact">galley</Link>
+            </div>
+        </nav>
+    </div>
+  );
+}
+
+export default Navbar;
