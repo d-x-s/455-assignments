ccb28f3117cdefe64368d9ceeacd3be805f55b4c
Davis Song <davissong01@gmail.com>
2023-06-25 11:42:22 -0700

    chore(A3): reorganize component and pages

--- a3/my-app/src/pages/Server.js
+++ a3/my-app/src/pages/Server.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Server from '../components/Server';
 import ServerForm from '../components/ServerForm';
-import ServerFilter from '../components/ServerFilter';
 
 export default function Contact() {
   return (
     <h1>
       <ServerForm />
       <Server />
-      <ServerFilter />
     </h1>
   );
 }
